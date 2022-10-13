959ba621dfb25a7c400f16a6be7346a20a26e54b
YuLim2 <jyulim0120@gmail.com>
2022-10-13 15:57:23 +0900

    ADD: Make code when user join

--- src/main/java/com/todoone/service/AuthService.java
+++ src/main/java/com/todoone/service/AuthService.java
@@ -1,14 +1,25 @@
 package com.todoone.service;
 
 import com.todoone.domain.User;
+import com.todoone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Random;
 
 @Service
 public class AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void encodePW(User user) {
         String enPw = passwordEncoder.encode(user.getPassword());
@@ -18,6 +29,17 @@ public class AuthService {
 
     public boolean encodePWForCheck(String cuPw, User user) {
         return passwordEncoder.matches(user.getPassword(), cuPw);
+    }
+
+    public int makeCode(User user) {
+        int code;
+        while (true) {
+            code = (int) (Math.random() * 99999) + 1;
+            Map<String, Object> isCode = userRepository.findByCode(code);
+            if (isCode == null) break;
+        }
+        user.setCode(code);
+        return code;
     }
 
 }
